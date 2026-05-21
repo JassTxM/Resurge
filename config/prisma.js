fda97f56b35b468c882498bf71f6e6b9fb74e443
JassTxM <jasstxm@gmail.com>
2026-05-21 19:16:07 +0530

    [PRISMA] | Deployement

--- config/prisma.js
+++ config/prisma.js
@@ -2,7 +2,14 @@ import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/index.js';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const connectionString = process.env.DATABASE_URL;
+
+// Render PostgreSQL requires SSL - add sslmode if not present
+const connectionStringWithSSL = connectionString && !connectionString.includes('sslmode')
+    ? `${connectionString}?sslmode=require`
+    : connectionString;
+
+const adapter = new PrismaPg({ connectionString: connectionStringWithSSL });
 
 const prisma = new PrismaClient({ adapter });
 
