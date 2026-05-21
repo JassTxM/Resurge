dcde2e7f5bc0d3595e2109dc6be13928efc1d414
JassTxM <jasstxm@gmail.com>
2026-05-21 18:45:56 +0530

    [PRISMA] | Deployement

--- config/prisma.js
+++ config/prisma.js
@@ -1,6 +1,9 @@
 import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/index.js';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
