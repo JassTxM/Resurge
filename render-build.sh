f40574e2630c983645bf830f90a2ace2e0258cf1
JassTxM <jasstxm@gmail.com>
2026-05-21 13:02:16 +0530

    [FULLSTACK] | Create Resurge Project

--- render-build.sh
+++ render-build.sh
@@ -16,6 +16,9 @@ echo ">>> Installing Python dependencies..."
 # Use break-system-packages in case Render is using Ubuntu 24 / Python 3.12+
 python3 -m pip install --break-system-packages -r requirements.txt || python3 -m pip install -r requirements.txt
 
+echo ">>> Downloading NLTK Data..."
+python3 -m nltk.downloader -d /opt/render/nltk_data punkt punkt_tab stopwords || python3 -m nltk.downloader punkt punkt_tab stopwords
+
 echo ">>> Generating Prisma Client..."
 npx prisma generate
 
